7b9ec70657178d382829bcab74d02b03a9ce7146
Primaaditya Redyananda <prima.redyananda@gmail.com>
2026-07-06 12:28:58 +0700

    add: favicon + title on index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home />
   },
 ]);
 
